fb001bc3b231200233e7bd1fc23844acb635ee5c
BrunoArrm <bruno.arrm@gmail.com>
2022-07-10 22:30:23 -0300

    tentativa de resolver o exercicio BEE1118.js

--- BEE1118.js
+++ BEE1118.js
@@ -21,22 +21,33 @@ for (media1.length; media1.length < 2; i++){
 // Imprime os resultados
 console.log('media = ' + ((media1[0] + media1[1]) / 2).toFixed(2));
 console.log('novo calculo (1-sim 2-nao)');
-// Trava o teste "novo calculo (1-sim 2-nao)" quando fim = 1
-let fim = 0;
-for (i; fim < 1; i++){
-    if (dados[i] <= 10 && dados[i] >= 0){
+
+function simOuNao (sn){
+    if (sn === 2){
+        console.log('Parar o programa = 2');
+    }
+    if (sn < 1 || sn > 2){
         console.log('novo calculo (1-sim 2-nao)');
-    } else if (dados[i] === 2){
-        console.log('laço 2');
-        break;
     }
 }
-// Realiza o cálculo da segunda média
-// for (media2.length; media2.length < 2; i++){
-//     if ( dados[i] <= 10 && dados[i] >= 0){
-//         media2.push(dados[i]);
-//     } else {
-//         console.log('nota invalida');
-//     }
-// }
-console.log(media2)
+function validaNota (vf){
+    if (vf <= 10 && vf >= 0){
+        media2.push(vf);
+    } else {
+        console.log('nota invalida');
+    }
+}
+
+for (i; i < lines.length; i++){
+    if (dados[i] === 1) {
+        i++;
+        validaNota(dados[i]);
+    } else {
+        simOuNao(dados[i]);
+        i++;
+    }
+}
+console.log('TESTE i = ' + i)
+console.log(media2);
+console.log('media = ' + ((media2[0] + media2[1]) / 2).toFixed(2));
+simOuNao(dados[i]);
